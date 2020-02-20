e378a1b9d8246f633f304853f648fc65db57f629
Scott Sawyer <ssawyer06@gmail.com>
2020-02-20 13:49:33 -0500

    Support for SQL Server named instances on Linux

--- common.py
+++ common.py
@@ -27,6 +27,8 @@ class PaperlessConfig:
 class JobBOSSConfig:
     def __init__(self, **kwargs):
         self.host = kwargs.get('host')
+        self.instance = kwargs.get('instance')
+        self.port = kwargs.get('port')
         self.name = kwargs.get('name')
         self.user = kwargs.get('user')
         self.password = kwargs.get('password')
@@ -66,6 +68,8 @@ def configure(test_mode=False):
 
     JOBBOSS_CONFIG = JobBOSSConfig(
         host=parser['JobBOSS']['host'],
+        instance=parser['JobBOSS'].get('instance'),
+        port=parser['JobBOSS'].get('port'),
         name=parser['JobBOSS']['name'],
         user=parser['JobBOSS']['user'],
         password=parser['JobBOSS']['password'],
@@ -79,5 +83,9 @@ def configure(test_mode=False):
     os.environ.setdefault('JOBBOSS_DB_NAME', JOBBOSS_CONFIG.name)
     os.environ.setdefault('JOBBOSS_DB_USERNAME', JOBBOSS_CONFIG.user)
     os.environ.setdefault('JOBBOSS_DB_PASSWORD', JOBBOSS_CONFIG.password)
+    if JOBBOSS_CONFIG.instance:
+        os.environ.setdefault('JOBBOSS_DB_INSTANCE', JOBBOSS_CONFIG.instance)
+    if JOBBOSS_CONFIG.port:
+        os.environ.setdefault('JOBBOSS_DB_PORT', JOBBOSS_CONFIG.port)
     if test_mode:
         os.environ.setdefault('JOBBOSS_TEST', '1')
